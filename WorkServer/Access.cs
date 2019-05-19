2f2ac998ba86c1395280915cf687f263cad9cd89
Konstantin Kulikov <HELLSPIRIT111@YANDEX.RU>
2019-05-20 00:19:47 +0700

    save all chages

---
+++ WorkServer/Access.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkServer
+{
+    class Access
+    {
+        public static string GetRightsForFile(string userName) {
+            string s = "";
+
+            return s;
+        }
+        public static string GetAllFilesForUser(string userName) {
+            string s = "";
+
+            return s;
+        }
+        public static bool CheckRightForFile(string fileName) {
+            bool check = false;
+
+            return check;
+        }
+    }
+}
